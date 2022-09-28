cb4bdb09ffb773a270fc7a52ac09dc64a9f7115a
sawwaraa <101964382+sawwaraa@users.noreply.github.com>
2022-09-28 21:10:14 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalken</h1>";
+        echo"nami abi teh sawwara kuawwamri
+            abi teh tinggal di purworejo
+            olah raga nu resep adalah
+            ngebucin babarengan sami meyku";
+    }
+}
---
+++ view-latihan1.php
@@ -0,0 +1,10 @@
+<html> <head>
+ <title>Latihan 1</title>
+ </head> <body>
+ Hallo Kawan.. Yuk Kita belajar web Programming..!!<br>
+ Nilai 1 = <?= $nilai1; ?>
+ Nilai 2 = <?= $nilai2; ?>
+ ini hasil nilai pemodelan dengan metode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
